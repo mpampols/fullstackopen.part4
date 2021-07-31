0caca469a414b47eb048b6829aea22124276146e
Marc Pampols <mpampols@mac-mpampols.local>
2021-07-31 22:11:23 +0200

    Finish 4.15

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 const mongoose = require('mongoose')
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
