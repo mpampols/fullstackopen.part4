a31c0d40455ba540d1f9cedaa93ace9f68cd2436
Marc Pampols <mpampols@mac-mpampols.local>
2021-08-01 13:53:10 +0200

    Finish 4.18

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ const cors = require('cors')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoose = require('mongoose')
 
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
